4adb99048c759b6414a4271799881f46fc2aa4fe
wodrow <wodrow451611cv@gmail.com>
2017-08-23 14:19:05 +0800

    update

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -16,7 +16,6 @@ class TestController extends Controller
     public function actionTest()
     {
         $serv = new \Swoole\Http\Server("127.0.0.1", 9502);
-
         $serv->on('Request', function($request, $response) {
             var_dump($request->get);
             var_dump($request->post);
@@ -24,12 +23,10 @@ class TestController extends Controller
             var_dump($request->files);
             var_dump($request->header);
             var_dump($request->server);
-
             $response->cookie("User", "Swoole");
             $response->header("X-Server", "Swoole");
             $response->end("<h1>Hello Swoole!</h1>");
         });
-
         $serv->start();
     }
 }
