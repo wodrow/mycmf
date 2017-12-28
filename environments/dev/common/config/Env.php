17c24a31aee831c2511d577dbac6421764e9b7d1
wodrow <wodrow451611cv@gmail.com>
2017-12-28 13:53:52 +0800

    update

--- environments/dev/common/config/Env.php
+++ environments/dev/common/config/Env.php
@@ -1,15 +1,15 @@
 <?php
 
-//namespace common\config;
-//
-//
-//use yii\base\Component;
-//
-//class Env extends Component
-//{
-//    const VERSION = '';
-//    const DOMAIN = DOMAIN;
-//    const HOME_URL = "http://".self::DOMAIN;
-//    const BACKEND_URL = self::HOME_URL."/backend";
-//    const API_URL = self::HOME_URL."/api";
-//}
+namespace common\config;
+
+
+use yii\base\Component;
+
+class Env extends Component
+{
+    const VERSION = '0.1';
+    const DOMAIN = DOMAIN;
+    const HOME_URL = "http://".self::DOMAIN;
+    const BACKEND_URL = self::HOME_URL."/backend";
+    const API_URL = self::HOME_URL."/api";
+}
