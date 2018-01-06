0d5c0f268fbcc89e998a5dc35974a9695fbee845
wodrow <wodrow451611cv@gmail.com>
2018-01-07 00:47:00 +0800

    update

--- console/modules/crawler/controllers/TieBaController.php
+++ console/modules/crawler/controllers/TieBaController.php
@@ -9,6 +9,10 @@
 namespace console\modules\crawler\controllers;
 
 
+use common\components\tools\ArrayHelper;
+use common\components\tools\Tools;
+use console\modules\crawler\models\TieBa;
+use QL\QueryList;
 use yii\console\Controller;
 
 class TieBaController extends Controller
@@ -22,11 +26,31 @@ class TieBaController extends Controller
     }
 
     /**
-     * php yii crawler/tie-ba/test
+     * php yii crawler/tie-ba/get-tie-ba-story
      * @param $url
      */
     public function actionGetTieBaStory($url)
     {
-        echo $url;
+        $ql = QueryList::getInstance()->get($url);
+        $title = $ql->find('title')->text();
+        $pages = $ql->find('.l_reply_num')->find('.red:eq(1)')->text();
+        $content = "";
+        for ($i = 1; $i <= $pages; $i++){
+            if ($i == 1){
+                $_ql = $ql;
+            }else{
+                $_ql = QueryList::getInstance()->get($url."?pn={$i}");
+            }
+            $contents = $_ql->find('.j_d_post_content')->texts();
+            foreach ($contents as $k => $v){
+                $x = $v;
+                if (TieBa::checkIsStory($x)){
+                    $content .= $x;
+                }else{
+                    $content .= "";
+                }
+            }
+        }
+        echo $content;
     }
 }
