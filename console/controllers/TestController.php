476b36ad185b9ac15b5588a3e82f8ae0659e6b53
wodrow <wodrow451611cv@gmail.com>
2018-05-04 16:56:27 +0800

    update

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -22,4 +22,24 @@ class TestController extends Controller
         var_dump(Env::DOMAIN);
         var_dump(\Yii::$app->user->identity->toArray());
     }
+
+    public function actionSwooleWebsocketStart()
+    {
+        $server = new \swoole_websocket_server("127.0.0.1", 9502);
+
+        $server->on('open', function($server, $req) {
+            echo "connection open: {$req->fd}\n";
+        });
+
+        $server->on('message', function($server, $frame) {
+            echo "received message: {$frame->data}\n";
+            $server->push($frame->fd, json_encode(["hello", "world"]));
+        });
+
+        $server->on('close', function($server, $fd) {
+            echo "connection close: {$fd}\n";
+        });
+
+        $server->start();
+    }
 }
