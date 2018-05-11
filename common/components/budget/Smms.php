23318ff8828cf619f31078736805e087f8eacccc
wodrow <wodrow451611cv@gmail.com>
2018-05-11 10:56:43 +0800

    update upload budget

--- common/components/budget/Smms.php
+++ common/components/budget/Smms.php
@@ -65,5 +65,36 @@ class Smms extends Bed
     }
 
     public function uploadFormUrl($url)
-    {}
+    {
+        $url = self::UPLOAD_URL;
+        $body = file_get_contents($url);
+        $client = new \GuzzleHttp\Client();
+        $r = $client->request('POST', $url, [
+            'multipart' => [
+                [
+                    'name'     => 'smfile',
+                    'contents' => $body,
+                ],
+            ],
+        ]);
+        $r = $r->getBody();
+        $r = json_decode($r);
+        if ($r->code!='success'){
+            throw new ErrorException(self::TITLE."上传失败");
+        }
+        $data = $r->data;
+        $api_resp = new ApiResp();
+        $api_resp->width = $data->width;
+        $api_resp->height = $data->height;
+        $api_resp->filename = $data->filename;
+        $api_resp->storename = $data->storename;
+        $api_resp->size = $data->size;
+        $api_resp->path = $data->path;
+        $api_resp->hash = $data->hash;
+        $api_resp->uploaded_at = $data->timestamp;
+        $api_resp->uploaded_ip = $data->ip;
+        $api_resp->url = $data->url;
+        $api_resp->delete_url = $data->delete;
+        return $api_resp;
+    }
 }
