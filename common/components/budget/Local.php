c924fbd3368252dd9aa5aa350bc41faca749f60a
wodrow-home-pc <wodrow451611cv@gmail.com>
2018-06-02 15:11:24 +0800

    init

---
+++ common/components/budget/Local.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wodrow
+ * Date: 18-6-2
+ * Time: 下午2:00
+ */
+
+namespace common\components\budget;
+
+
+class Local extends Bed
+{
+    public function uploadFormUrl($url)
+    {
+        // TODO: Implement uploadFormUrl() method.
+    }
+
+    public function uploadLocalFile($file)
+    {
+        // TODO: Implement uploadLocalFile() method.
+    }
+
+    public function deleteByUrl($delete_url)
+    {
+        // TODO: Implement deleteByUrl() method.
+    }
+}
--- common/models/db/base/Document.php
+++ common/models/db/base/Document.php
@@ -8,25 +8,27 @@ use Yii;
  * This is the model class for table "{{%document}}".
  *
  * @property string $id
- * @property string $name
- * @property string $title
- * @property int $type
+ * @property string $name 名称
+ * @property string $title 标题
+ * @property int $type 类型
  * @property string $created_by
  * @property int $created_at
  * @property string $updated_by
  * @property int $updated_at
- * @property int $status
- * @property string $content
+ * @property int $status 状态
+ * @property string $content 主体内容
+ * @property string $thumb_main 主缩略图
  * @property string $reprint_from 转载于
  * @property int $abs_sort 固定排序
  *
  * @property User $createdBy
  * @property User $updatedBy
+ * @property Files $thumbMain
  */
 class Document extends \yii\db\ActiveRecord
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public static function tableName()
     {
@@ -34,36 +36,38 @@ class Document extends \yii\db\ActiveRecord
     }
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function rules()
     {
         return [
             [['name', 'title', 'type', 'created_at', 'updated_at', 'status'], 'required'],
-            [['type', 'created_by', 'created_at', 'updated_by', 'updated_at', 'status', 'abs_sort'], 'integer'],
+            [['type', 'created_by', 'created_at', 'updated_by', 'updated_at', 'status', 'thumb_main', 'abs_sort'], 'integer'],
             [['content'], 'string'],
             [['name', 'title', 'reprint_from'], 'string', 'max' => 200],
-            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
-            [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['updated_by' => 'id']],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+            [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['updated_by' => 'id']],
+            [['thumb_main'], 'exist', 'skipOnError' => true, 'targetClass' => Files::className(), 'targetAttribute' => ['thumb_main' => 'id']],
         ];
     }
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function attributeLabels()
     {
         return [
             'id' => Yii::t('app', 'ID'),
-            'name' => Yii::t('app', 'Name'),
-            'title' => Yii::t('app', 'Title'),
-            'type' => Yii::t('app', 'Type'),
+            'name' => Yii::t('app', '名称'),
+            'title' => Yii::t('app', '标题'),
+            'type' => Yii::t('app', '类型'),
             'created_by' => Yii::t('app', 'Created By'),
             'created_at' => Yii::t('app', 'Created At'),
             'updated_by' => Yii::t('app', 'Updated By'),
             'updated_at' => Yii::t('app', 'Updated At'),
-            'status' => Yii::t('app', 'Status'),
-            'content' => Yii::t('app', 'Content'),
+            'status' => Yii::t('app', '状态'),
+            'content' => Yii::t('app', '主体内容'),
+            'thumb_main' => Yii::t('app', '主缩略图'),
             'reprint_from' => Yii::t('app', '转载于'),
             'abs_sort' => Yii::t('app', '固定排序'),
         ];
@@ -74,7 +78,7 @@ class Document extends \yii\db\ActiveRecord
      */
     public function getCreatedBy()
     {
-        return $this->hasOne(User::class, ['id' => 'created_by']);
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
     }
 
     /**
@@ -82,6 +86,14 @@ class Document extends \yii\db\ActiveRecord
      */
     public function getUpdatedBy()
     {
-        return $this->hasOne(User::class, ['id' => 'updated_by']);
+        return $this->hasOne(User::className(), ['id' => 'updated_by']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getThumbMain()
+    {
+        return $this->hasOne(Files::className(), ['id' => 'thumb_main']);
     }
 }
